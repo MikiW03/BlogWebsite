53a5ef4b2fbde5106cd12dfd523d31d8484fe8cc
MikiW03 <mikiwiniecki@gmail.com>
2022-06-26 21:50:57 +0200

    Make posts generate with data from api

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,60 @@
 <template>
   <h1>Blog</h1>
-  <PostItem></PostItem>
+  <div id="posts">
+    <PostItem v-for="post in posts" :key="post.id" :title="post.title" :body="post.body" :tags="post.tags"
+      :reactions="post.reactions">
+    </PostItem>
+  </div>
 </template>
 
 <script>
 import PostItem from './components/PostItem.vue'
 
 export default {
+  data() {
+    return {
+      posts: {},
+    }
+  },
   components: {
     PostItem
+  },
+  created() {
+    fetch("https://dummyjson.com/posts")
+      .then(response => response.json())
+      .then(data => {
+        ({ posts: this.posts } = data)
+      })
   }
 }
 
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+
+:root {
+  --clr-primary: #1c1c1c;
+  --clr-bg: #121212;
+  --clr-font: #ffffff;
+  --clr-accent: #5e91ff;
+}
+
+* {
+  margin: 0;
+}
+
+body {
+  margin: 0;
+  background-color: var(--clr-bg);
+  color: var(--clr-font);
+  font-family: "Inter";
+  line-height: 1.4;
+}
+
+#app {
+  width: clamp(40vw, 500px, 90vw);
+  margin-inline: auto;
+  margin-block: 0;
+}
 </style>
--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,15 +1,63 @@
 <template>
-    <h2>{{ name }}</h2>
-    <p>{{ description }}</p>
-    <img :src="imgSrc" alt="post's photo">
+    <div class="post">
+        <h2>{{ title }}</h2>
+        <p>{{ body }}</p>
+        <ul class="tags">
+            <li v-for="(tag, i) in tags" :key="i">
+                {{ tag }}
+            </li>
+        </ul>
+        <p class="reactions">
+            <img class="like-icon" src="https://i.pinimg.com/originals/39/44/6c/39446caa52f53369b92bc97253d2b2f1.png"
+                alt="like-icon">
+            {{ reactions }}
+        </p>
+    </div>
 </template>
 
 <script>
 export default {
     props: {
-        name: String,
-        description: String,
-        imgSrc: String
+        title: String,
+        body: String,
+        tags: Array,
+        reactions: Number
     }
 }
 </script>
+
+<style>
+.post {
+    background-color: var(--clr-primary);
+    border-radius: .2em;
+    padding: 1em 1em;
+    margin-block: 1em;
+}
+
+.post>.tags {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1em;
+    padding: 0;
+    list-style-type: none;
+    margin-block: 1em;
+    margin-inline: 0;
+}
+
+.post>.tags>li {
+    padding: .1em .5em;
+    border-radius: .2em;
+    background: var(--clr-accent);
+}
+
+.post>.reactions {
+    display: flex;
+    gap: .2em;
+    align-items: center;
+    height: 30px;
+}
+
+.post>.reactions>img {
+    max-height: 100%;
+}
+</style>
